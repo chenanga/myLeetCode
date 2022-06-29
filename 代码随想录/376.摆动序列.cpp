7a5e9513ae300bec0f2bf26cfead3a75f7723723
chenang <blogca@qq.com>
2022-06-29 15:38:49 +0800

    376. 摆动序列

--- 代码随想录/376.摆动序列.cpp
+++ 代码随想录/376.摆动序列.cpp
@@ -64,26 +64,49 @@ using namespace std;
 //     }
 // };
 
-//使用滚动数组2
+// //使用滚动数组2
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         int len = nums.size();
+//         //动态规划
+//         //memo[i][0] 为第i个元素结尾的降序的最大摆动序列，降序指的是上一个元素到元素i是降序
+//         //memo[i][1] 为第i个元素结尾的升序的最大摆动序列，升序指的是上一个元素到元素i是升序
+//         //当nums[i] > nums[i-1],则以i结尾的的降序序列的最大值只能是以i-1结尾的最大值
+//         int preUp = 1, preDown = 1;
+
+//         for (int i = 1; i < len; ++ i) {
+//             if (nums[i] < nums[i - 1]) 
+//                 preDown = max(preUp + 1, preDown);
+
+//             else if (nums[i] > nums[i - 1]) 
+//                 preUp = max(preDown + 1, preUp);
+
+//         }
+//         return max(preDown, preUp);
+//     }
+// };
+
+
+// 贪心
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int len = nums.size();
-        //动态规划
-        //memo[i][0] 为第i个元素结尾的降序的最大摆动序列，降序指的是上一个元素到元素i是降序
-        //memo[i][1] 为第i个元素结尾的升序的最大摆动序列，升序指的是上一个元素到元素i是升序
-        //当nums[i] > nums[i-1],则以i结尾的的降序序列的最大值只能是以i-1结尾的最大值
-        int preUp = 1, preDown = 1;
+        int result = 1;
+        int curDiff, preDiff = 0;
 
         for (int i = 1; i < len; ++i) {
-            if (nums[i] < nums[i - 1])
-                preDown = max(preUp + 1, preDown);
-
-            else if (nums[i] > nums[i - 1])
-                preUp = max(preDown + 1, preUp);
+            curDiff = nums[i] - nums[i - 1];
+            if (curDiff > 0 && preDiff <= 0 || curDiff < 0 && preDiff >= 0) {
+                result++;
+                preDiff = curDiff;  // 只有在满足当前条件时候，才去更新prediff
+            }
 
         }
-        return max(preDown, preUp);
+
+
+        return result;
     }
 };
 

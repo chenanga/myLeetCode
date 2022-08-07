89a93406ad8179d46b10350619b6fc08f2a96f6f
chenang <blogca@qq.com>
2022-08-07 11:52:54 +0800

    206. 反转链表

--- 代码随想录/206. 反转链表.cpp
+++ 代码随想录/206. 反转链表.cpp
@@ -93,3 +93,20 @@ private:
     }
 
 };
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         // 边缘条件判断
+//         if(head == NULL) return NULL;
+//         if (head->next == NULL) return head;
+
+//         // 递归调用，翻转第二个节点开始往后的链表
+//         ListNode *last = reverseList(head->next);
+//         // 翻转头节点与第二个节点的指向
+//         head->next->next = head;
+//         // 此时的 head 节点为尾节点，next 需要指向 NULL
+//         head->next = NULL;
+//         return last;
+//     }
+// }; 

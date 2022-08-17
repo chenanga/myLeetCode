fa525d9de92e533cfcbc97a455b9abb9d64f9c5d
chenang <blogca@qq.com>
2022-08-17 10:49:43 +0800

    700. 二叉搜索树中的搜索

--- 代码随想录/700. 二叉搜索树中的搜索.cpp
+++ 代码随想录/700. 二叉搜索树中的搜索.cpp
@@ -11,15 +11,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //  class Solution {
+ // public:
+ //     TreeNode* searchBST(TreeNode* root, int val) {
+ //         if (root == nullptr) return nullptr;
+ //         if (root->val == val) return root;
+ //         TreeNode* left = searchBST(root->left, val);
+ //         if (left != nullptr)
+ //             return left;
+ //         else
+ //             return searchBST(root->right, val);
+ //     }
+ // };
+
+ // ÓÅ»¯
+ // class Solution {
+ // public:
+ //     TreeNode* searchBST(TreeNode* root, int val) {
+ //         if (root == nullptr) return nullptr;
+ //         if (root->val == val) return root;
+ //         else if (root->val < val)
+ //             return searchBST(root->right, val);
+ //         else
+ //             return searchBST(root->left, val);
+ //     }
+ // };
+
+ // µü´ú
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
         if (root == nullptr) return nullptr;
-        if (root->val == val) return root;
-        TreeNode* left = searchBST(root->left, val);
-        if (left != nullptr)
-            return left;
-        else
-            return searchBST(root->right, val);
+        while (root != nullptr) {
+            if (root->val == val) return root;
+            else if (root->val < val)
+                root = root->right;
+            else
+                root = root->left;
+        }
+        return nullptr;
     }
 };
